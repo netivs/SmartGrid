b85389bb1ce49cefd3e1a7d32cd6146b1b44c364
Hieu Nguyen <minhhieun4@gmail.com>
2019-10-19 15:30:28 +0700

    fix minor things in arima

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import random
 if __name__ == "__main__":
     # read file skip datetime column
     data_load_area = read_csv('data/data_autocorrelation.csv', usecols=range(1,30))
-    x, y = prepare_data(data_load_area, 4)
-    # print(x,y)
-
+    nodes = 29
+    l = 100
+    r = 0.8
+    h = 1
+    p = 0.6
+    model_arima(data_load_area, nodes, l, r, h, p)
     # df = read_csv('data/data_grid.csv')
     # print(df.groupby('is_verified').size())
     # 86,62%. FALSE: 70608. TRUE: 527952
--- model/arima.py
+++ model/arima.py
@@ -8,33 +8,35 @@ import numpy as np
 # h: The number of time-steps need to be predicted
 # p: The percentage size of train data compared to the whold data
 def model_arima(data, nodes, l, r = 0.8, h = 1, p = 0.6):
-    binary_matrix = binary_matrix(r, nodes, l)
+    binary_mat = binary_matrix(r, nodes, l)
     # run predict for 29 nodes
     for load_area in LOAD_AREAS:
         series = data[[load_area]].squeeze('columns')
+        if series.empty == True:
+            continue
         X = series.values
         size = int(len(X) * p)
         train, test = X[0:size], X[size:len(X)]
         history = [x for x in train]
+
+        # Cant pass history[-l:] directly to auto_arima
+        histotry = history[-l:]
         predictions = list()
         gt = []
-        for t in range(len(train) - l - h):
-
+        for t in range(len(test)-h+1):
             # Only use l time-steps as inputs
-            model = pm.auto_arima(history[-l:], error_action = 'ignore', seasonal = True, m = l)
+            model = pm.auto_arima(history, error_action = 'ignore', seasonal = True, m = l)
             yhat = model.predict(n_periods = h)
-            
             predictions.append(yhat)
             gt.append(test[t:t+h])
             for i in range(h):
-                if binary_matrix[LOAD_AREAS.index(load_area)][t+size + i] == 1:
+                # compare if test i have m_i = 1 or not
+                if binary_mat[LOAD_AREAS.index(load_area)][t + h + i] == 1:
                     # Update the data if verified == True
                     history.append(test[t+i])
                 else:
                     # Otherwise use the predicted data
                     history.append(yhat[i])
-
-            # print('predicted=%f, expected=%f' % (yhat, obs))
 
         predictions = np.stack(predictions, axis=0)
         gt = np.stack(gt, axis=0)
