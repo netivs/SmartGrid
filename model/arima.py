a10739da7717c8a0d20378d5ce1e2f764b08bb7c
vananle <vananle1993@gmail.com>
2019-10-19 14:35:24 +0900

    arima

--- model/arima.py
+++ model/arima.py
@@ -2,6 +2,7 @@ import pmdarima as pm
 from utils.constant import LOAD_AREAS
 from utils.model import cal_error, binary_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 
 # l: The number of time-steps used for each prediction
 # h: The number of time-steps need to be predicted
@@ -16,26 +17,28 @@ def model_arima(data, nodes, l, r = 0.8, h = 1, p = 0.6):
         train, test = X[0:size], X[size:len(X)]
         history = [x for x in train]
         predictions = list()
+        gt = []
         for t in range(len(train) - l - h):
 
-            # An
+            # Only use l time-steps as inputs
             model = pm.auto_arima(history[-l:], error_action = 'ignore', seasonal = True, m = l)
-            output = model.predict(n_periods = h)
+            yhat = model.predict(n_periods = h)
             
-            ####### STUCK HERE. dont know what to do next when having H of outputs
-            yhat = output[0]
             predictions.append(yhat)
-            obs = test[t:t+h]
+            gt.append(test[t:t+h])
             for i in range(h):
                 if binary_matrix[LOAD_AREAS.index(load_area)][t+size + i] == 1:
+                    # Update the data if verified == True
                     history.append(test[t+i])
                 else:
+                    # Otherwise use the predicted data
                     history.append(yhat[i])
-            ##########
 
-            print('predicted=%f, expected=%f' % (yhat, obs))
-        
-        cal_error(test, predictions)
+            # print('predicted=%f, expected=%f' % (yhat, obs))
+
+        predictions = np.stack(predictions, axis=0)
+        gt = np.stack(gt, axis=0)
+        cal_error(gt.flatten(), predictions.flatten())
 
         # plot
         plt.plot(test)
